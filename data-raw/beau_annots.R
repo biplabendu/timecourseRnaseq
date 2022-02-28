5023034a9c46d52dba3b05dfd324fc8c58343c26
Biplabendu Das <biplabendu.das@gmail.com>
2022-02-28 09:56:44 -0500

    added beau gene names

--- data-raw/beau_annots.R
+++ data-raw/beau_annots.R
@@ -2,11 +2,19 @@
 
 library(tidyverse)
 
+beau_names <-
+  read.csv("./data-raw/beau_protein_gene_names_ncbi.csv",
+           header=T, stringsAsFactors = F, na.strings = c(NA,""," ")) %>%
+  as_tibble() %>%
+  select(gene_name = locus_tag, gene_desc=protein_name)
+
 beau_annots <-
   read.csv(paste0("./data-raw/beau_annots_robin_ncbi.csv"),
            header=T, stringsAsFactors = F, na.strings = c(NA,""," ")) %>%
   as_tibble() %>%
-  mutate(gene_desc="not_available")
+  left_join(beau_names, by="gene_name") %>%
+  select(gene_name, gene_desc, everything())
 
 
-usethis::use_data(beau_annots)
+
+usethis::use_data(beau_annots, overwrite = T)
